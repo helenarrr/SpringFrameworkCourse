71a2f1657d1fd96261e537567436e1f5012db39c
Elena Radchenko <elena.radchenko.9@mail.ru>
2024-03-13 23:33:02 +0300

    git commit -m "first commit"

---
+++ src/main/java/ru/gb/practice_spring/student/Student.java
@@ -0,0 +1,21 @@
+package ru.gb.practice_spring.student;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    long id;
+
+    private String name;
+
+    private String nameOfGroup;
+
+    private static long curId = 0;
+
+    public Student(String name, String nameOfGroup) {
+        this.id = curId++;
+        this.name = name;
+        this.nameOfGroup = nameOfGroup;
+    }
+}
